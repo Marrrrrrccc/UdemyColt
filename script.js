dd507d163cb20f5b04f94d055ee4d491e2606213
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-06-12 11:52:22 +0800

    annoy loop

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-console.log("-10 -> 19");
-var number = -10;
-while (number <= 19) {
-  console.log(number);
-  number++;
+var answer = prompt("Are we there yet?");
+while (answer.indexOf("yes") === -1) {
+  var answer = prompt("Are we there yet?");
 }
-console.log("even between 10 -> 40");
-var even = 10;
-while (even <= 40) {
-  if (even % 2 === 0) {
-    console.log(even);
-  }
-  even++;
-}
-console.log("Odd between 300 ->333");
-var odd = 300;
-while (odd <= 333) {
-  if (odd % 2 !== 0) {
-    console.log(odd);
-  }
-  odd++;
-}
-console.log("divisible");
-var divisible = 5;
-while (divisible <= 50) {
-  if (divisible % 5 === 0 && divisible % 3 === 0) {
-    console.log(divisible);
-  }
-  divisible++;
-}
+alert("Yay! we made it");
