d074a6a4bf657e6d8757e9e13d429d371aa57ebb
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-06-12 11:37:57 +0800

    loop probset

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-age = 16;
-if (age < 0) {
-  console.log("Negative");
-} else if (age === 21) {
-  console.log("21");
-} else if (age % 2 === 1) {
-  console.log("odd");
-} else if (age % Math.sqrt(age) === 0) {
-  console.log("Perfect square");
+console.log("-10 -> 19");
+var number = -10;
+while (number <= 19) {
+  console.log(number);
+  number++;
+}
+console.log("even between 10 -> 40");
+var even = 10;
+while (even <= 40) {
+  if (even % 2 === 0) {
+    console.log(even);
+  }
+  even++;
+}
+console.log("Odd between 300 ->333");
+var odd = 300;
+while (odd <= 333) {
+  if (odd % 2 !== 0) {
+    console.log(odd);
+  }
+  odd++;
+}
+console.log("divisible");
+var divisible = 5;
+while (divisible <= 50) {
+  if (divisible % 5 === 0 && divisible % 3 === 0) {
+    console.log(divisible);
+  }
+  divisible++;
 }
