e01623c4ec3d0df42c4470ccf9ff5d0a19dc56cf
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-06-13 11:11:58 +0800

    javascript function

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-console.log("-10 -> 19");
-for (var i = -10; i <= 19; i++) {
-  console.log(i);
-}
-console.log("even");
-for (var i = 10; i <= 40; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
+function isEven(x) {
+  if (x % 2 === 0) {
+    return true;
+  } else {
+    return false;
   }
 }
-console.log("odd");
-for (var i = 300; i <= 333; i++) {
-  if (i % 2 !== 0) {
-    console.log(i);
+// isEven(4);
+function factorial(num) {
+  var result = 1;
+  for (var i = 2; i <= num; i++) {
+    result *= i;
   }
+  return result;
 }
-console.log("divisible");
-for (var d = 5; d <= 50; d++) {
-  if (d % 5 === 0 && d % 3 === 0) {
-    console.log(d);
-  }
+function kebabToSnake(str) {
+  var newStr = str.replace(/-/g, "_");
+  return newStr;
 }
-// console.log("Odd between 300 ->333");
-// var odd = 300;
-// while (odd <= 333) {
-//   if (odd % 2 !== 0) {
-//     console.log(odd);
-//   }
-//   odd++;
-// }
-// console.log("divisible");
-// var divisible = 5;
-// while(divisible<=50){
-//     if(divisible % 5 === 0 && divisible%3===0){
-//         console.log(divisible);
-//     }
-//     divisible++
-// }
