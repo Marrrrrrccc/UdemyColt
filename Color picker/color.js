d0a8446a0b0dad303928c6eea060603e947b53d7
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-06-18 19:20:30 +0800

    color picker

---
+++ Color picker/color.js
@@ -0,0 +1,46 @@
+var color = generateRandomColors(6);
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#colorDisplay");
+var pickedColor = pickedColor();
+colorDisplay.textContent = pickedColor;
+var message = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+for (var i = 0; i < color.length; i++) {
+  squares[i].style.backgroundColor = color[i];
+  squares[i].addEventListener("click", function () {
+    var clickedColor = this.style.backgroundColor;
+    if (clickedColor === pickedColor) {
+      message.textContent = "Correct";
+      changeColors(clickedColor);
+      h1.style.backgroundColor = pickedColor;
+    } else {
+      this.style.backgroundColor = "#232323";
+      message.textContent = "Try again";
+    }
+  });
+}
+function changeColors(color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+function pickedColor() {
+  var random = Math.floor(Math.random() * color.length);
+  return color[random];
+}
+
+function generateRandomColors(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+}
