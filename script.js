23fb296ea70dcf028d1f287f54cfc51a80ef9672
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-06-22 11:22:44 +0800

    To do(jQuery)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("div").css("backgroundColor", "purple");
-$(".highlight").css("width", "200px");
-$("#third").css("border", "2px orange solid");
-$("div:first-of-type").css("color", "pink");
+$("button").on("click", function () {
+  $("div").slideToggle();
+});
+
